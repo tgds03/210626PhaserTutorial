7e25fd8b8504e42a629b8dfb91cf0a81cba6a3ef
tgds03 <xyzuct6800@naver.com>
2021-06-26 14:31:24 +0000

    World Building & The Platforms https://phaser.io/tutorials/making-your-first-phaser-3-game/part3 https://phaser.io/tutorials/making-your-first-phaser-3-game/part4

--- scripts/main.js
+++ scripts/main.js
@@ -2,6 +2,13 @@ var config = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 300 },
+            debug: false
+        }
+    },
     scene: {
         preload: preload,
         create: create,
@@ -13,13 +20,21 @@ var game = new Phaser.Game(config);
 
 function preload () {
     this.load.image('sky', 'assets/sky.png');
-    this.load.image('platform', 'assets/platform.png');
+    this.load.image('ground', 'assets/platform.png');
     this.load.image('bomb', 'assets/bomb.png');
     this.load.image('star', 'assets/star.png');
 }
 
+var platforms;
+
 function create () {
     this.add.image(400, 300, 'sky');
+
+    platforms = this.physics.add.staticGroup();
+    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
+    platforms.create(600, 400, 'ground');
+    platforms.create(50, 250, 'ground');
+    platforms.create(750, 220, 'ground');
 }
 
 function update ()
