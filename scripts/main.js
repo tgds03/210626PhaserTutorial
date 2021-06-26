ffab1c4b24f8766b09efbcbb4e891226ad6cc9f2
tgds03 <xyzuct6800@naver.com>
2021-06-26 13:56:06 +0000

    https://phaser.io/tutorials/making-your-first-phaser-3-game/part2 Loading Assets

--- scripts/main.js
+++ scripts/main.js
@@ -11,12 +11,15 @@ var config = {
 
 var game = new Phaser.Game(config);
 
-function preload ()
-{
+function preload () {
+    this.load.image('sky', 'assets/sky.png');
+    this.load.image('platform', 'assets/platform.png');
+    this.load.image('bomb', 'assets/bomb.png');
+    this.load.image('star', 'assets/star.png');
 }
 
-function create ()
-{
+function create () {
+    this.add.image(400, 300, 'sky');
 }
 
 function update ()
