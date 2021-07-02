cacc46285fbefdef9911235d47d8759b0eb8b4a4
tgds03 <xyzuct6800@naver.com>
2021-07-02 14:23:41 +0000

    The Player https://phaser.io/tutorials/making-your-first-phaser-3-game/part5

--- scripts/main.js
+++ scripts/main.js
@@ -23,6 +23,7 @@ function preload () {
     this.load.image('ground', 'assets/platform.png');
     this.load.image('bomb', 'assets/bomb.png');
     this.load.image('star', 'assets/star.png');
+    this.load.spritesheet('dude', 'assets/dude.png', { frameWidth: 32, frameHeight: 48 });
 }
 
 var platforms;
@@ -35,6 +36,30 @@ function create () {
     platforms.create(600, 400, 'ground');
     platforms.create(50, 250, 'ground');
     platforms.create(750, 220, 'ground');
+        
+    player = this.physics.add.sprite(100, 450, 'dude');
+    player.setBounce(0.2);
+    player.setColliderWorldBounds(true);
+
+    this.anims.create({
+        key: 'left',
+        frames: this.anims.generateFrameNumbers( 'dude', {start: 0, end: 3} ),
+        frameRate: 10,
+        repeat: -1
+    });
+
+    this.anims.create( {
+        key: 'turn',
+        frames: [ {key: 'dude', frame: 4 } ],
+        frameRate: 20
+    });
+
+    this.anims.create({
+        key: 'right',
+        frames: this.anims.generateFrameNumbers( 'dude', {start: 5, end: 8} ),
+        frameRate: 10,
+        repeat: -1
+    });
 }
 
 function update ()
